5f733ba3c03a541b9f9196f447b7b398752abad2
Grenolf004 <Ghost_Yriy@ukr.net>
2024-04-28 20:50:05 +0300

    check

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import css from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={css.footer}>
+          Made by Yurii Leibiuk GoIT 2024
+        </footer>
+  )
+}
+
+export default Footer
